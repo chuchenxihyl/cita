15aa99193139aed23aefe3110e359284d7520c97
lycbitbucket <liuyucheng@cryptape.com>
2017-09-13 15:00:23 +0800

    fix some compile error in zeromq

--- share_libs/pubsub/src/lib.rs
+++ share_libs/pubsub/src/lib.rs
@@ -14,6 +14,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+use dotenv::dotenv;
 #[cfg(feature = "pubsub_kafka")]
 extern crate pubsub_kafka;
 #[cfg(feature = "pubsub_rabbitmq")]
